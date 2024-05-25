fa575aa5ff12a3e02eb4b64d0a7f027448f6fe96
huxiaoheng <1548564358@qq.com>
2024-05-25 18:50:10 +0800

    提交项目架构：1.0.0

---
+++ main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * @author huXiaoHeng on 2024/5/25.
+ */
+public class Main {
+    public static void main(String[] args) {
+        String version = "1.0.0";
+        System.out.println("提交项目架构" + version);
+    }
+}
